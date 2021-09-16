71a03fdbb50d893f2b8c3dfec25ae2c6b250adfe
myasoedas <myasoedas@yandex.ru>
2021-09-16 21:44:52 +0300

    feat: Сверстал первый экран мобильной версии сайта: меню с логотипом и оффер с двумя кнопками

--- src/components/articleOffer/ArticleOffer.js
+++ src/components/articleOffer/ArticleOffer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import articleOfferImage from '../../images/articleOfferKopilkaDobra.png'
 
 class ArticleOffer extends React.Component {
   constructor(props) {
@@ -8,7 +9,12 @@ class ArticleOffer extends React.Component {
 
   render() {
     return (
-      <article className="articleOffer">
+      <article className="article">
+        <img className="articleImageTitle" src={articleOfferImage} alt="Люди несут свои сердца в большую копилку"/>
+        <h1 className="articleTitle">Помогите детям осуществить мечту стать здоровыми</h1>
+        <p className="articleText">Помощь в сборе средств на дорогостоящее лечение</p>
+        <button class="buttonWithColor" name ="buttonWantToHelp" type="button">Хочу помочь</button>
+        <button class="buttonWithoutColor" name ="buttonWithoutColor" type="button">Нужна помощь</button>
 
 
 

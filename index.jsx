3f7da4d4bc514a5388bdf2e858fa5b0880f7dbc2
myasoedas <myasoedas@yandex.ru>
2021-09-23 21:22:33 +0300

    feat: Сверстали попап Menu

--- index.jsx
+++ index.jsx
@@ -2,12 +2,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { appState: false };
+    this.popupIsopen = "overlayIsOpened";
   }
 
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header onMenuButton={false} onCloseButton={false} />
         <Main />
       </div>
     );
@@ -17,8 +18,25 @@ class App extends React.Component {
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { headerState: false };
+    this.state = {
+      onMenuButton: this.props.onMenuButton,
+      onCloseButton: this.props.onCloseButton,
+    };
   }
+
+  handleOnMenuButton = () => {
+    this.setState({
+      onMenuButton: !this.state.onMenuButton,
+    });
+  };
+
+  handleOnCloseButton = () => {
+    this.setState({
+      onMenuButton: this.props.onMenuButton,
+      onCloseButton: this.props.onCloseButton,
+    });
+  };
+
   render() {
     return (
       <header className="header">
@@ -28,7 +46,29 @@ class Header extends React.Component {
           <img className="headerLogoDobro" src="./images/logo_dobro_vo_blago.svg" alt="Добро во благо"/>
           <img className="headerLogoFond" src="./images/logo_blagotvoritelni_fond.svg" alt="Благотворительный фонд"/>
         </div>
-        <img className="headerMenuButton" src="./images/menu_=.svg" alt="Кнопка меню ="/>
+        <img onClick={this.handleOnMenuButton} className="headerMenuButton" src="./images/menu_=.svg" alt="Кнопка меню ="/>
+        <div className={`overlay overlayMenuMobile ${this.state.onMenuButton ? 'overlayIsOpened' : ''}`}>
+            <div className="popup">
+              <button onClick={this.handleOnCloseButton} className="popupButtonClose" name ="popupButtonClose" type="button"></button>
+              <nav className="popupNavigation">
+                <ul className="popupListConteiner">
+                  <li className="popupListItem"><a className="popupListLink" href = "#">О фонде</a></li>
+                  <li className="popupListItem"><a className="popupListLink" href = "#">Как помочь</a></li>
+                  <li className="popupListItem"><a className="popupListLink" href = "#">Нуждаются в помощи</a></li>
+                  <li className="popupListItem"><a className="popupListLink" href = "#">Кому помогли</a></li>
+                  <li className="popupListItem"><a className="popupListLink" href = "#">Команда</a></li>
+                  <li className="popupListItem"><a className="popupListLink" href = "#">Как отправить перевод</a></li>
+                  <li className="popupListItem"><a className="popupListLink" href = "#">Документы фонда</a></li>
+                  <li className="popupListItem"><a className="popupListLink" href = "#">Отчеты</a></li>
+                </ul>
+              </nav>
+              <ul className="popupContactList">
+                <li className="popupListItem"><p className="popupListText">+7(926)444-22-30</p></li>
+                <li className="popupListItem"><p className="popupListText">Приём звонков: с 9:00 до 18:00 (пн-пт)</p></li>
+                <li className="popupListItem"><p className="popupListText">help@dvbfond.ru</p></li>
+              </ul>
+            </div>
+        </div>
       </header>
     );
   }
@@ -57,6 +97,7 @@ class Section extends React.Component {
     return (
       <section className="section">
         <ArticleOffer />
+        
       </section>
     );
   }
@@ -70,11 +111,11 @@ class ArticleOffer extends React.Component {
   render() {
     return (
       <article className="article">
-        <img className="articleImageTitle" src="./images/articleOfferKopilkaDobra.png" alt="Люди несут свои сердца в большую копилку"/>
+        <img className="articleImageTitle" src="./images/articleOfferKopilkaDobra.svg" alt="Люди несут свои сердца в большую копилку"/>
         <h1 className="articleTitle">Помогите детям осуществить мечту стать здоровыми</h1>
         <p className="articleText">Помощь в сборе средств на дорогостоящее лечение</p>
-        <button class="buttonWithColor" name ="buttonWantToHelp" type="button">Хочу помочь</button>
-        <button class="buttonWithoutColor" name ="buttonWithoutColor" type="button">Нужна помощь</button>
+        <button className="buttonWithColor" name ="buttonWantToHelp" type="button">Хочу помочь</button>
+        <button className="buttonWithoutColor" name ="buttonWithoutColor" type="button">Нужна помощь</button>
       </article>
     );
   }
